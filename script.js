9630b318dfb9099a0d553fddf4cb039acb1cc9ca
Nogitsune00 <analuisa062007@gmail.com>
2025-10-27 18:20:02 -0300

    feat: adiciona alerta de confirmação ao tentar mudar de página com formulário preenchido

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 let count = 1;
-let text_special = document.getElementById("especial_desc")
-let img_special = document.getElementById("especial_img")
 let forward = document.getElementById("forward_special")
 let backward = document.getElementById("backward_special")
+let iName = document.getElementById("nome")
+let iEmail = document.getElementById("email")
+let iTelephone = document.getElementById("telefone")
+let iMessage = document.getElementById("mensagem")
+let changePage = document.getElementsByTagName("a")
 
 setInterval(function(){
     nextImage();
 },5000)
+
+for (let i = 0; i < changePage.length; i++) {
+    changePage[i].addEventListener('click', function(event){
+        if(iName.value || iEmail.value || iTelephone.value || iMessage.value){
+            let exit = showConfirm();
+            if(!exit){
+                event.preventDefault();
+            }
+        }
+    });
+}
 
 forward.addEventListener('click', function(){
     if(count > 3){
@@ -14,7 +28,6 @@ forward.addEventListener('click', function(){
     }else{
         count += 1;
     }
-    
     showImage();
 });
 
@@ -36,6 +49,8 @@ function nextImage(){
 };
 
 function showImage(){
+    let text_special = document.getElementById("especial_desc")
+    let img_special = document.getElementById("especial_img")
     switch (count) {
         case 1:
             text_special.innerHTML = "Buquê de flores"
@@ -54,10 +69,12 @@ function showImage(){
             img_special.src = "img/arranjo_branco-removebg.png"
             break;
         default:
-           
+            text_special.innerHTML = "Buquê de flores"
+            img_special.src = "img/rosasvermelhasjornal-removebg.png"
+            break;
     }
 };
 
-    
-
-
+function showConfirm(){
+    return confirm("Ao sair da página pode ser que o seu progresso no formulário seja perdido. \n Tem certeza que deseja sair?");
+}
